92bac2c177abe71db70d10553239034b0e047fdb
Sebastian Braun <sebastian@elmnt.de>
2014-10-02 05:13:23 +0200

    minor update

--- js/bmf-calendar.js
+++ js/bmf-calendar.js
@@ -51,7 +51,7 @@
             return (((year % 4)==0) && ((year % 100)!=0) || ((year % 400)==0));
         }
 
-        base.getDaysInMonth = function(month,year) {
+        base.getDaysInMonth = function(month, year) {
             var days;
             if (month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12) {
                 days = 31;
@@ -59,7 +59,7 @@
             else if (month==4 || month==6 || month==9 || month==11) {
                 days = 30;
             }
-            else if (month==2 && CalendarNamespace.isLeapYear(year)) {
+            else if (month==2 && base.isLeapYear(year)) {
                 days = 29;
             }
             else {
@@ -87,7 +87,76 @@
         }
 
         base.buildDateField = function() {
-            base.datefield.html('<table class="table-condensed"><thead><tr><th class="prev">‹</th><th colspan="5" class="switch">February 2012</th><th class="next">›</th></tr><tr><th class="dow">Su</th><th class="dow">Mo</th><th class="dow">Tu</th><th class="dow">We</th><th class="dow">Th</th><th class="dow">Fr</th><th class="dow">Sa</th></tr></thead><tbody><tr><td class="day  old">29</td><td class="day  old">30</td><td class="day  old">31</td><td class="day ">1</td><td class="day ">2</td><td class="day ">3</td><td class="day ">4</td></tr><tr><td class="day ">5</td><td class="day ">6</td><td class="day ">7</td><td class="day ">8</td><td class="day ">9</td><td class="day ">10</td><td class="day ">11</td></tr><tr><td class="day  active">12</td><td class="day ">13</td><td class="day ">14</td><td class="day ">15</td><td class="day ">16</td><td class="day ">17</td><td class="day ">18</td></tr><tr><td class="day ">19</td><td class="day ">20</td><td class="day ">21</td><td class="day ">22</td><td class="day ">23</td><td class="day ">24</td><td class="day ">25</td></tr><tr><td class="day ">26</td><td class="day ">27</td><td class="day ">28</td><td class="day ">29</td><td class="day  new">1</td><td class="day  new">2</td><td class="day  new">3</td></tr><tr><td class="day  new">4</td><td class="day  new">5</td><td class="day  new">6</td><td class="day  new">7</td><td class="day  new">8</td><td class="day  new">9</td><td class="day  new">10</td></tr></tbody></table>');
+            var table = $('<table class="table-condensed">');
+            base.drawCalendar(table);
+            base.datefield.children().remove();
+            base.datefield.append(table);
+//          base.datefield.html('<table class="table-condensed"><thead><tr><th class="prev">‹</th><th colspan="5" class="switch">February 2012</th><th class="next">›</th></tr><tr><th class="dow">Su</th><th class="dow">Mo</th><th class="dow">Tu</th><th class="dow">We</th><th class="dow">Th</th><th class="dow">Fr</th><th class="dow">Sa</th></tr></thead><tbody><tr><td class="day  old">29</td><td class="day  old">30</td><td class="day  old">31</td><td class="day ">1</td><td class="day ">2</td><td class="day ">3</td><td class="day ">4</td></tr><tr><td class="day ">5</td><td class="day ">6</td><td class="day ">7</td><td class="day ">8</td><td class="day ">9</td><td class="day ">10</td><td class="day ">11</td></tr><tr><td class="day  active">12</td><td class="day ">13</td><td class="day ">14</td><td class="day ">15</td><td class="day ">16</td><td class="day ">17</td><td class="day ">18</td></tr><tr><td class="day ">19</td><td class="day ">20</td><td class="day ">21</td><td class="day ">22</td><td class="day ">23</td><td class="day ">24</td><td class="day ">25</td></tr><tr><td class="day ">26</td><td class="day ">27</td><td class="day ">28</td><td class="day ">29</td><td class="day  new">1</td><td class="day  new">2</td><td class="day  new">3</td></tr><tr><td class="day  new">4</td><td class="day  new">5</td><td class="day  new">6</td><td class="day  new">7</td><td class="day  new">8</td><td class="day  new">9</td><td class="day  new">10</td></tr></tbody></table>');
+        }
+
+        base.drawCalendar = function(el, month, year) {
+            var today = new Date();
+            var todayDay = today.getDate();
+            var todayMonth = today.getMonth()+1;
+            var todayYear = today.getFullYear();
+
+            thead = $('<thead>');
+            tbody = $('<tbody>');
+
+            el.append(thead, tbody);
+
+            month = parseInt(month);
+            year = parseInt(year);
+
+            var tr = $('<tr>');
+            tbody.append(tr);
+            tr.append('<td>'); // weeknumber
+            for (var i = 0; i < 7; i++) {
+                tr.append('<td>'+ base.daysOfWeek[(i + base.firstDayOfWeek) % 7] +'</td>');
+            }
+
+            var startingPos = new Date(year, month-1, 1 - base.firstDayOfWeek).getDay();
+            var days = base.getDaysInMonth(month, year);
+
+            var tr = $('<tr>');
+            tbody.append(tr);
+            tr.append('<td>');
+
+            // empty days 
+            for (var i = 0; i < startingPos; i++) {
+                tr.append('<td class="noday"></td>');
+            }
+
+            var currentDay = 1;
+            for (var i = startingPos; currentDay <= days; i++) {
+                if (i%7 == 0 && currentDay != 1) {
+                    tr = $('<tr>');
+                    thead.append(tr);
+                    tr.append('<td>'); // weekday
+                }
+
+                var td = $('<td>' + currentDay +'</td>');
+
+//              if ((currentDay==todayDay) && (month==todayMonth) && (year==todayYear)) {
+//                  todayClass='today';
+//              } else {
+//                  todayClass='';
+//              }
+//
+//              // use UTC function; see above for explanation.
+//              if (isSelectedMonth && currentDay == selected.getUTCDate()) {
+//                  if (todayClass != '') todayClass += " ";
+//                  todayClass += "selected";
+//              }
+//
+//              var cell = quickElement('td', tableRow, '', 'class', todayClass);
+//
+//              quickElement('a', cell, currentDay, 'href', 'javascript:void(' + callback + '('+year+','+month+','+currentDay+'));');
+                currentDay++;
+            }
+
+            thead.append('<tr><th colspan="8">head</th></tr>');
+            tbody.append('<tr><td colspan="8">test</td></tr>');
         }
 
     }; // end erp.calendar
